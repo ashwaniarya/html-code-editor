8638cf2f988da05e3506b051e7fff85c8fd390af
Ashwani Arya <ashwaniparker@gmail.com>
2021-06-27 14:13:02 +0530

    Changed default html

--- src/constants.js
+++ src/constants.js
@@ -16,7 +16,7 @@ body {
 <h1>This is a Heading</h1>
 <p>This is a paragraph.</p>
 <p>Edit the code in the window to the left, and click "Run" to view the result.</p>
-<img src="avatar.png" alt="Avatar" style="width:200px">
+<img src="https://images.pexels.com/photos/1089440/pexels-photo-1089440.jpeg?auto=compress%26cs=tinysrgb%26dpr=3%26h=360%26w=720" alt="Avatar" style="width:200px">
 
 </body>
 </html>`;
